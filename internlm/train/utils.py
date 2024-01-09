f39e031b606da98460fac65493a25de9860890b8
huangting4201 <1538303371@qq.com>
2024-01-09 11:59:32 +0800

    fix(train/util.py): fix embed_head param group compute norm blocked

--- internlm/train/utils.py
+++ internlm/train/utils.py
@@ -138,9 +138,9 @@ def split_params_into_different_groups_for_optimizer_with_new_partition_strategy
         pgroup["params"] = origin_params
         pgroup["optimizer_mode"] = ParallelMode.ZERO1
 
-    # param groups may contain empty groups, such as fp32
-    if len(new_groups["embed_head"]["params"]) > 0:
-        param_groups.extend(new_groups.values())
+    # param groups may contain empty groups, such as embed_head
+    # if len(new_groups["embed_head"]["params"]) > 0:
+    param_groups.extend(new_groups.values())
 
     # print(f"ht debug params_groups after split default len:{len(param_groups[0]['params'])}", flush=True)
     # print(f"ht debug params_groups after split embed_head len:{len(param_groups[1]['params'])}", flush=True)
